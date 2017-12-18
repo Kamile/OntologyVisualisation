b22c05a614af177ecf42d48f9c252a26f7f49361
kamile <km662@cam.ac.uk>
2017-12-18 21:20:18 +0000

    Define Concept Diagram Creator to deal with layout constraints

--- src/concrete/ConceptDiagramCreator.java
+++ src/concrete/ConceptDiagramCreator.java
@@ -1,23 +1,42 @@
 package concrete;
 
+import abstractDescription.AbstractConceptDiagramDescription;
+import icircles.abstractDescription.AbstractCurve;
 import icircles.abstractDescription.AbstractDescription;
+import icircles.concreteDiagram.CircleContour;
 import icircles.concreteDiagram.DiagramCreator;
 import icircles.decomposition.DecompositionStrategy;
 import icircles.recomposition.RecompositionStrategy;
+import icircles.util.CannotDrawException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class ConceptDiagramCreator extends DiagramCreator {
+/***
+ * Layout algorithms for well-formedness of concept and property diagrams.
+ *
+ * Here we re-write the DiagramCreator from iCircles to remove split curves and apply
+ * all of the layout constraints as described in the success criteria.
+ */
+public class ConceptDiagramCreator {
 
     ArrayList<BoxContour> boxes;
+    ArrayList<CircleContour> circles;
+    HashMap<AbstractCurve, CircleContour> map;
 
-    public ConceptDiagramCreator(AbstractDescription ad) {
-        super(ad);
+    public ConceptDiagramCreator(AbstractConceptDiagramDescription abstractDescription) {
+
     }
 
-    public ConceptDiagramCreator(AbstractDescription ad, DecompositionStrategy decomp_strategy, RecompositionStrategy recomp_strategy) {
-        super(ad, decomp_strategy, recomp_strategy);
+    public ConceptDiagramCreator(AbstractConceptDiagramDescription abstractDescription, DecompositionStrategy decomp_strategy, RecompositionStrategy recomp_strategy) {
+
     }
 
+    public ConcreteConceptDiagram createDiagram(int size) throws CannotDrawException {
+        circles = new ArrayList<CircleContour>();
+        ConcreteConceptDiagram result = null;
 
+        return result;
+
+    }
 }
--- src/concrete/ConcreteConceptDiagram.java
+++ src/concrete/ConcreteConceptDiagram.java
@@ -1,40 +1,37 @@
 package concrete;
 
 
-import icircles.concreteDiagram.CircleContour;
-import icircles.concreteDiagram.ConcreteSpider;
-import icircles.concreteDiagram.ConcreteZone;
+import abstractDescription.AbstractConceptDiagramDescription;
+import icircles.concreteDiagram.*;
 
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
 /***
  * Build on ConcreteDiagram in iCircles, adding boxes around entities,
  * allowing for different edges.
  */
-public class ConcreteConceptDiagram {
+public class ConcreteConceptDiagram extends ConcreteDiagram {
 
-    ArrayList<CircleContour> circles;
     ArrayList<BoxContour> boxes;
-    ArrayList<ConcreteZone> shadedZones;
-    ArrayList<ConcreteZone> unshadedZones;
-    ArrayList<ConcreteSpider> spiders;
-    private Font font;
 
     public ConcreteConceptDiagram(ArrayList<BoxContour> boxes,
                                   ArrayList<CircleContour> circles,
                                   ArrayList<ConcreteZone> shadedZones,
                                   ArrayList<ConcreteZone> unshadedZones,
                                   ArrayList<ConcreteSpider> spiders) {
+        super(new Rectangle2D.Double(), circles, shadedZones, unshadedZones, spiders);
         this.boxes = boxes;
-        this.circles = circles;
-        this.shadedZones = shadedZones;
-        this.unshadedZones = unshadedZones;
-        this.spiders = spiders;
     }
 
-    public ArrayList<CircleContour> getCircles() {
-        return circles;
+    public ArrayList<BoxContour> getBoxes() {
+        return boxes;
+    }
+
+    public static ConcreteConceptDiagram makeConcreteDiagram(AbstractConceptDiagramDescription abstractDescription, int size) {
+        ConceptDiagramCreator cdc = new ConceptDiagramCreator(abstractDescription);
+        return cdc.createDiagram(size);
     }
 
 }
