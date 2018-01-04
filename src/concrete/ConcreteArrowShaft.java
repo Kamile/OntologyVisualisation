fa06b1736653a7267c369750f095a59a5037199e
kamile <km662@cam.ac.uk>
2018-01-04 22:15:33 +0000

    Add concrete description of arrow shaft

--- src/concrete/ConcreteArrowShaft.java
+++ src/concrete/ConcreteArrowShaft.java
@@ -2,16 +2,34 @@ package concrete;
 
 import org.apache.log4j.Logger;
 
+import java.awt.*;
+
 /**
  * Arrows represent properties.
  * Where arrow targets are anonymous, properties can be represented
  * as anonymous by using a dashed line.
  */
 public class ConcreteArrowShaft {
-    Logger logger = Logger.getLogger(ConcreteArrowShaft.class.getName());
+    final static float dash[] = {5.0f};
+    final static BasicStroke dashed =  new BasicStroke(0.1f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 5.0f, dash, 0.0f);
+
+    private ConcreteArrowHead from;
+    private ConcreteArrowHead to;
+    private boolean isAnonymous;
+
+    public ConcreteArrowHead getFrom() {
+        return from;
+    }
+
+    public ConcreteArrowHead getTo() {
+         return to;
+    }
+
+    public boolean getIsAnonymous() {
+        return isAnonymous;
+    }
 
     public double checksum() {
-        double result = 0.0;
-        return result;
+        return 1.2 * from.checksum() + 2.2 * to.checksum();
     }
 }
