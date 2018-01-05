6c69b668fe2538724254cc7cc5938ea27784c97d
kamile <km662@cam.ac.uk>
2018-01-05 17:56:29 +0000

    Return 2D graphics object with arrow head drawn

--- src/concrete/ConcreteArrowHead.java
+++ src/concrete/ConcreteArrowHead.java
@@ -2,6 +2,7 @@ package concrete;
 
 import org.apache.log4j.Logger;
 
+import java.awt.*;
 import java.awt.geom.Ellipse2D;
 
 /**
@@ -13,16 +14,19 @@ import java.awt.geom.Ellipse2D;
 public class ConcreteArrowHead {
     Logger logger = Logger.getLogger(ConcreteArrowHead.class.getName());
 
-    private static double THETA = Math.PI/6;
-    private static final double HEAD_LENGTH = 2;
+    private static double ARROW_ANGLE = Math.PI/2;
+    private static final double HEAD_LENGTH = 20;
+    private static final BasicStroke stroke =  new BasicStroke(0.1f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 5.0f);
 
     private double x;
     private double y;
     private ConcreteArrow arrow;
+    private boolean isTarget; // target has arrowhead, source doesn't
 
-    public ConcreteArrowHead(double x, double y) {
+    public ConcreteArrowHead(double x, double y, boolean isTarget) {
         this.x = x;
         this.y = y;
+        this.isTarget = isTarget;
     }
 
     public double getX() {
@@ -41,6 +45,14 @@ public class ConcreteArrowHead {
         this.y = y;
     }
 
+    public boolean getIsTarget() {
+        return isTarget;
+    }
+
+    void setIsTarget(boolean isTarget) {
+        this.isTarget = isTarget;
+    }
+
     public ConcreteArrow getArrow() {
         return arrow;
     }
@@ -50,16 +62,21 @@ public class ConcreteArrowHead {
     }
 
     // Return a polyline for the vee
-    public void getArrowHead() {
+    public Graphics2D getArrowHead() {
+        if (isTarget) {
+
+        }
+        return null;
 
     }
 
     /**
      * Set coordinates for given arrowHead
      */
-    public void getArrowHead(Ellipse2D.Double arrowHead) {
-        arrowHead.x = x;
-        arrowHead.y = y;
+    public void getArrowHead(Graphics2D g2) {
+        if (!isTarget) {
+            return;
+        }
     }
 
     public double checksum() {
