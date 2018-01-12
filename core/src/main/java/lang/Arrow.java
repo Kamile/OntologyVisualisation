bafd0253107fe8211d942e05e49c53172a9ddabb
kamile <km662@cam.ac.uk>
2018-01-12 18:41:09 +0000

    Implement Comparable methods for Arrow

--- core/src/main/java/lang/Arrow.java
+++ core/src/main/java/lang/Arrow.java
@@ -1,11 +1,20 @@
 package lang;
 
-public class Arrow {
+import org.mockito.internal.matchers.Null;
+
+import java.io.Serializable;
+
+import static com.sun.xml.internal.bind.v2.schemagen.Util.equal;
+
+public class Arrow implements Comparable<Arrow>, ConceptDiagramElement, Serializable{
 
     private final static String NO_LABEL = "";
     private String label;
     private String source;
     private String target;
+    private boolean hashInvalid = true;
+    private int hash;
+
 
     public Arrow(String source, String target) {
         this(NO_LABEL, source, target);
@@ -29,4 +38,51 @@ public class Arrow {
         return target;
     }
 
+    @Override
+    public int compareTo(Arrow o) {
+        if (o == null) {
+            throw new NullPointerException();
+        }
+
+        if (this == o) {
+            return 0;
+        } else if (this.source != o.source){
+            return this.source.compareTo(o.source);
+        } else {
+            return this.target.compareTo(o.target);
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        } else if (obj instanceof Arrow) {
+            Arrow a = (Arrow) obj;
+            return equal(label, a.label) && equal(source, a.source) && equal(target, a.target);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        if (hashInvalid) {
+            hash = (label == null ? 0 : label.hashCode())
+                    + (source == null ? 0: source.hashCode())
+                    + (target == null ? 0: target.hashCode());
+            hashInvalid = false;
+        }
+        return hash;
+    }
+
+    public void toString(Appendable sb) {
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        toString(sb);
+        return sb.toString();
+    }
 }
